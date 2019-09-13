9e4794dc5bec78eeb031d825daca29f3ccdce770
Wendy <alteredco@gmail.com>
2019-09-13 14:15:59 +0200

    add data parameters to routes

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const { data } = require('../data/portfolioData.json');
+const { projects } = data;
 
 router.get('/', (req, res) => {
-  res.render('index', {name: "Wendy Kakuda", tagline: "Artist, Mom and Busy Bee"});
+  res.render('index', {name: 'Wendy Kakuda', tagline: 'Artist, Mom and Busy Bee',
+  portfolioDesc: 'Treehouse TechDegree Projects',
+  projectLink: '/projects/0'
+  });
 });
 
 module.exports = router;
