d480a626bd74df91e16b315024afe463f9b03e26
Wendy <alteredco@gmail.com>
2019-09-16 13:16:45 +0200

    update static server path

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const path = require('path');
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static('public'));
+app.use('/static', express.static(path.join(__dirname, './public')))
 
 app.set('view engine' , 'pug');
 
