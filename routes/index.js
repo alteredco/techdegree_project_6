883737b2dc65ec4f5cb587fd8974c9bb6f1d6a70
Wendy <alteredco@gmail.com>
2019-09-17 12:01:49 +0200

    add images and image links

--- routes/index.js
+++ routes/index.js
@@ -4,18 +4,7 @@ const { data } = require('../data/portfolioData.json');
 const { projects } = data;
 
 router.get('/', (req, res) => {
-  const id  =  projects[0];
-  const { project_name } = id;
-  const title = project_name;
-  const {image_urls} = id;
-  const {thumbnail} = image_urls;
-  const projectImg = thumbnail;
-  res.render('index', {name: 'Wendy Kakuda', tagline: 'Full Stack Developer',
-  portfolioDesc: 'All the works',
-  projectLink: `/projects/0`,
-  title,
-  projectImg
-  });
+  res.render('index', {projects});
 });
 
 module.exports = router;
